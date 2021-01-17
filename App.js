81aaa723ee775cb77cbb5c00968563aa88db79b5
antonio36alv <ant.36alv@gmail.com>
2021-01-17 00:04:16 -0500

    fix: now uses dotenv file

--- App.js
+++ App.js
@@ -6,13 +6,13 @@ import * as firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCDBHd46BBKjqbwgfpbgLDfyWf5k_Bqwl4",
-  authDomain: "instagram-clone-demo-1416b.firebaseapp.com",
-  projectId: "instagram-clone-demo-1416b",
-  storageBucket: "instagram-clone-demo-1416b.appspot.com",
-  messagingSenderId: "988149112645",
-  appId: "1:988149112645:web:948e69a8a75838d98ee82c",
-  measurementId: "G-XFT8P7QTBE"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID, 
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 import { NavigationContainer } from "@react-navigation/native";
