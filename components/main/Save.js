7f71ff52cefe7d30f33e1f435ef9c91357c63f71
antonio36alv <ant.36alv@gmail.com>
2021-01-25 17:57:23 -0500

    initial commit

---
+++ components/main/Save.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native'
+
+export default function Save() {
+    return (
+        <View>
+
+        </View>
+    )
+}
